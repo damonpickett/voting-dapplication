9ee197844fc757452f94df6d08928d1e788033e8
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-14 15:04:51 -0700

    first commit, just notes so far

---
+++ ballot-vote.sol
@@ -0,0 +1,8 @@
+// making a voting contract
+
+// 1. we want the ability to accept proposals and store them
+// proposal: their name, number
+
+// 2. voters and voting ability
+// keep track of voting
+// check voters are authenticated to vote

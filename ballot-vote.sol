3ac0d4e74b150b5e1958227be20b6493e504bd75
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-14 15:25:29 -0700

    added Voter and Proposal structs

--- ballot-vote.sol
+++ ballot-vote.sol
@@ -1,3 +1,5 @@
+pragma solidity >= 0.7.0 < 0.9.0
+
 // making a voting contract
 
 // 1. we want the ability to accept proposals and store them
@@ -6,3 +8,21 @@
 // 2. voters and voting ability
 // keep track of voting
 // check voters are authenticated to vote
+
+contract Ballot {
+    // struct is a method to create your own data type
+
+    // voters: voted, access to vote, vote index
+
+    struct Voter {
+        uint vote
+        bool voted;
+        uint weight;
+    }
+
+    struct Proposal {
+        // you can use bytes instead of string because it saves on gas fees
+        bytes32 name; // the name of each proposal
+        uint voteCount; // number of accumulated votes
+    }
+}
